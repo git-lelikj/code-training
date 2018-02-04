93ff5b51823c245e3ddb509623d9ba0a7236a786
Alexei Jemkov <Alexei.Jemkov@advantechwireless.com>
2018-02-04 16:31:57 +0200

    Added new solutions

--- main.cpp
+++ main.cpp
@@ -1214,7 +1214,7 @@ int main()
 Please note that it's Function problem i.e.
 you need to write your solution in the form of Function(s) only.
 Driver Code to call/invoke your function would be added by GfG's Online Judge.*/
-
+#if 0
 #include <iostream>
 #include <unordered_map>
 
@@ -1304,3 +1304,51 @@ int main()
 
     return 0;
 }
+#endif
+
+#if 0
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct A;
+struct B;
+
+struct A
+{
+    A() { cout << "A::A()\n"; }
+    ~A() { cout << "A::~A()\n"; }
+    shared_ptr<B> b_ = nullptr;
+};
+
+struct B
+{
+    B() { cout << "B::B()\n"; }
+    ~B() { cout << "B::~B()\n"; }
+    weak_ptr<A> a_;
+};
+
+int main()
+{
+    shared_ptr<A> a = make_shared<A>();
+    shared_ptr<B> b = make_shared<B>();
+    a->b_ = b;
+    b->a_ = a;
+    cout << "counts a: " << a.use_count() << ", b: " << b.use_count() << ", a.b_: " << a->b_.use_count() << ", b.a_: " << b->a_.use_count() << endl;
+}
+#endif
+
+#if 0
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int main()
+{
+    int a[] = {1,2,3,4,5,6};
+
+    cout << a[1+3] << "," << (1+3)[a] << endl;
+}
+#endif
