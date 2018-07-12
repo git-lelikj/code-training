695a340f440ea102569b0d827c8d0296fcb738cd
Alexei Jemkov <Alexei.Jemkov@advantechwireless.com>
2018-07-12 16:33:09 +0300

    Add new solutions

--- main.cpp
+++ main.cpp
@@ -2,174 +2,197 @@
 #include <map>
 #include <memory>
 #include <functional>
+#include "public.h"
+#include "singleton.h"
+#include "class_factory.h"
 using namespace std;
 
 namespace Common
 {
-
-enum CLSID_type {
-    CLSID_Class1 = 0,
-    CLSID_Class2
-};
-
-class ICommon
-{
-public:
-    virtual ~ICommon() {}
-};
-
-class IInterface1: virtual public ICommon
-{
-public:
-    virtual void f1() = 0;
-};
-
-class IInterface2: virtual public ICommon
-{
-public:
-    virtual void f2() = 0;
-};
-
-class IInterface3: virtual public ICommon
-{
-public:
-    virtual void f3() = 0;
-};
-
-class Class1: public IInterface1
-{
-public:
-    void f1() { cout << "[Class1::f1]\n"; }
-    ~Class1() { cout << "[Class1::~Class1]\n"; }
-};
-
-class Class2:   public IInterface2,
-                public IInterface3
-{
-public:
-    void f2() { cout << "[Class2::f2]\n"; }
-    void f3() { cout << "[Class2::f3]\n"; }
-    ~Class2() { cout << "[Class2::~Class2]\n"; }
-};
-
-template<typename Class, typename Base>
-shared_ptr<Base> create_shared()
-{
-    return dynamic_pointer_cast<Base>(make_shared<Class>());
+    template<typename T, typename Base>
+    shared_ptr<Base> create_singleton()
+    {
+        return dynamic_pointer_cast<Base>(T::get_instance());
+    }
 }
 
-template<typename Interface, typename Base>
-shared_ptr<Interface> query_interface(shared_ptr<Base> ptr)
+namespace Cls_factory_test
 {
-    return dynamic_pointer_cast<Interface>(ptr);
+    using namespace Common;
+
+//    enum CLSID_type {
+//        CLSID_Class1 = 0,
+//        CLSID_Class2
+//    };
+
+//    class ICommon
+//    {
+//    public:
+//        virtual ~ICommon() {}
+//    };
+
+    class IInterface1: virtual public ICommon
+    {
+    public:
+        virtual void f1() = 0;
+    };
+
+    class IInterface2: virtual public ICommon
+    {
+    public:
+        virtual void f2() = 0;
+    };
+
+    class IInterface3: virtual public ICommon
+    {
+    public:
+        virtual void f3() = 0;
+    };
+
+    class Class1: public IInterface1
+    {
+    public:
+        void f1() { cout << "[Class1::f1]\n"; }
+        ~Class1() { cout << "[Class1::~Class1]\n"; }
+    };
+
+    class Class2:   public IInterface2,
+                    public IInterface3
+    {
+    public:
+        void f2() { cout << "[Class2::f2]\n"; }
+        void f3() { cout << "[Class2::f3]\n"; }
+        ~Class2() { cout << "[Class2::~Class2]\n"; }
+    };
+
+    using Class_factory = Factory<CLSID_type, ICommon, std::function<std::shared_ptr<ICommon>(void)>>;
 }
 
-template <
-    typename Class_id,
-    typename Base,
-    typename Factory_function
->
-class Factory
+namespace Singleton_test
 {
-public:
-    bool register_class(Class_id clsid, Factory_function creator)
+    using namespace Common;
+
+    class IInterface: virtual public ICommon
     {
-        return (associations_.insert({clsid, creator}).second);
-    }
+    public:
+        virtual void f1() = 0;
+    };
 
-    std::shared_ptr<Base> create_object(Class_id clsid)
+    class Class:    public Common::Shared_singleton<Class>,
+                    public IInterface
     {
-        auto it = associations_.find(clsid);
-        if (it != associations_.end())
-            return it->second();
-        else
-            return nullptr;
-    }
-
-    template<typename Interface>
-    std::shared_ptr<Interface> create_object(Class_id clsid)
-    {
-        auto it = associations_.find(clsid);
-        if (it != associations_.end())
-            return dynamic_pointer_cast<Interface>(it->second());
-        else
-            return nullptr;
-    }
-
-private:
-    map<Class_id, Factory_function> associations_;
-};
-
-using Class_factory = Factory<CLSID_type, ICommon, std::function<std::shared_ptr<ICommon>(void)>>;
-
+    public:
+        ~Class() { cout << "[Class::~Class]\n"; }
+        void f1() { cout << "[Class:f1]\n"; }
+    private:
+        Class() {}
+        friend class Common::Shared_singleton<Class>;
+    };
 }
 
 int main(int argc, char *argv[])
 {
     using namespace Common;
+    using namespace Cls_factory_test;
+    using namespace Singleton_test;
 
-    Class_factory factory;
+    cout << "[main]: class factory test =============================================\n";
+    {
+        Class_factory factory;
 
-    cout << (factory.register_class(CLSID_Class1, &create_shared<Class1, ICommon>) == false?"[main]: failed to register CLSID_Class1\n":"[main]: registered CLSID_Class1\n");
-    cout << (factory.register_class(CLSID_Class2, &create_shared<Class2, ICommon>) == false?"[main]: failed to register CLSID_Class2\n":"[main]: registered CLSID_Class2\n");
-//    cout << (factory.register_class(CLSID_Class1, &create_shared<Class1, ICommon>) == false?"[main]: failed to register CLSID_Class1\n":"[main]: registered CLSID_Class1\n");
-
-    {
-        std::shared_ptr<IInterface1> c = factory.create_object<IInterface1>(CLSID_Class1);
-        if (c) {
-            cout << "[main]: calling Class1::f1...\n";
-            c->f1();
-            cout << "[main]: ptr use_count: " << c.use_count() << endl;
-        }
-        else
-            cout << "[main]: cant create pointer to interface: Class1::IInterface1\n";
-    }
-    {
-        std::shared_ptr<IInterface2> c = factory.create_object<IInterface2>(CLSID_Class1);
-        if (c) {
-            cout << "[main]: calling Class1::f2...\n";
-            c->f2();
-            cout << "[main]: ptr use_count: " << c.use_count() << endl;
-        }
-        else
-            cout << "[main]: cant create pointer to interface: Class1::IInterface2\n";
-    }
-    {
-        std::shared_ptr<IInterface2> i2 = factory.create_object<IInterface2>(CLSID_Class2);
-        if (i2) {
-            cout << "[main]: calling Class2::f2...\n";
-            i2->f2();
-            cout << "[main]: ptr use_count: " << i2.use_count() << endl;
-            cout << "[main]: query Class2::IInterface3...\n";
-            shared_ptr<IInterface3> i3 = query_interface<IInterface3>(i2);
-            if (i3) {
-                cout << "[main]: calling Class2::f3...\n";
-                i3->f3();
-                cout << "[main]: ptr i2 use_count: " << i2.use_count() << endl;
-                cout << "[main]: ptr i3 use_count: " << i3.use_count() << endl;
+        cout << (factory.register_class(CLSID_Class1, &create_shared<Class1, ICommon>) == false?"[main]: failed to register CLSID_Class1\n":"[main]: registered CLSID_Class1\n");
+        cout << (factory.register_class(CLSID_Class2, &create_shared<Class2, ICommon>) == false?"[main]: failed to register CLSID_Class2\n":"[main]: registered CLSID_Class2\n");
+    //    cout << (factory.register_class(CLSID_Class1, &create_shared<Class1, ICommon>) == false?"[main]: failed to register CLSID_Class1\n":"[main]: registered CLSID_Class1\n");
+        {
+            std::shared_ptr<IInterface1> c = factory.create_object<IInterface1>(CLSID_Class1);
+            if (c) {
+                cout << "[main]: calling Class1::f1...\n";
+                c->f1();
+                cout << "[main]: ptr use_count: " << c.use_count() << endl;
             }
             else
-                cout << "[main]: cant query Class2::IInterface3\n";
+                cout << "[main]: cant create pointer to interface: Class1::IInterface1\n";
         }
-        else
-            cout << "[main]: cant create pointer to interface: Class2::IInterface2\n";
+        {
+            std::shared_ptr<IInterface2> c = factory.create_object<IInterface2>(CLSID_Class1);
+            if (c) {
+                cout << "[main]: calling Class1::f2...\n";
+                c->f2();
+                cout << "[main]: ptr use_count: " << c.use_count() << endl;
+            }
+            else
+                cout << "[main]: cant create pointer to interface: Class1::IInterface2\n";
+        }
+        {
+            std::shared_ptr<IInterface2> i2 = factory.create_object<IInterface2>(CLSID_Class2);
+            if (i2) {
+                cout << "[main]: calling Class2::f2...\n";
+                i2->f2();
+                cout << "[main]: ptr use_count: " << i2.use_count() << endl;
+                cout << "[main]: query Class2::IInterface3...\n";
+                shared_ptr<IInterface3> i3 = query_interface<IInterface3>(i2);
+                if (i3) {
+                    cout << "[main]: calling Class2::f3...\n";
+                    i3->f3();
+                    cout << "[main]: ptr i2 use_count: " << i2.use_count() << endl;
+                    cout << "[main]: ptr i3 use_count: " << i3.use_count() << endl;
+                }
+                else
+                    cout << "[main]: cant query Class2::IInterface3\n";
+            }
+            else
+                cout << "[main]: cant create pointer to interface: Class2::IInterface2\n";
+        }
+        {
+            std::shared_ptr<IInterface3> c = factory.create_object<IInterface3>(CLSID_Class2);
+            if (c) {
+                cout << "[main]: calling Class2::f3...\n";
+                c->f3();
+                cout << "[main]: ptr use_count: " << c.use_count() << endl;
+            }
+            else
+                cout << "[main]: cant create pointer to interface: Class2::IInterface3\n";
+        }
+
+        {
+            shared_ptr<IInterface1> i1 = factory.create_object<IInterface1>(CLSID_Class1);
+            shared_ptr<IInterface2> i2 = factory.create_object<IInterface2>(CLSID_Class2);
+            shared_ptr<IInterface3> i3 = query_interface<IInterface3>(i2);
+        }
     }
+    cout << "[main]: singleton test =============================================\n";
     {
-        std::shared_ptr<IInterface3> c = factory.create_object<IInterface3>(CLSID_Class2);
-        if (c) {
-            cout << "[main]: calling Class2::f3...\n";
-            c->f3();
-            cout << "[main]: ptr use_count: " << c.use_count() << endl;
-        }
-        else
-            cout << "[main]: cant create pointer to interface: Class2::IInterface3\n";
+//        Class* c = new Class;
+        shared_ptr<Class> s1 = Class::get_instance();
+        cout << "[main]: Class use count s1: " << s1.use_count() << endl;
+        shared_ptr<Class> s2 = Class::get_instance();
+        cout << "[main]: Class use count s1: " << s1.use_count() << ", s2 use count: " << s2.use_count() << endl;
     }
 
+    cout << "[main]: proceeding...\n";
     {
-        shared_ptr<IInterface1> i1 = factory.create_object<IInterface1>(CLSID_Class1);
-        shared_ptr<IInterface2> i2 = factory.create_object<IInterface2>(CLSID_Class2);
-        shared_ptr<IInterface3> i3 = query_interface<IInterface3>(i2);
+        shared_ptr<Class> s1 = Class::get_instance();
+        cout << "[main]: Class use count s1: " << s1.use_count() << endl;
+        shared_ptr<Class> s2 = Class::get_instance();
+        cout << "[main]: Class use count s1: " << s1.use_count() << ", s2 use count: " << s2.use_count() << endl;
     }
 
+    cout << "[main]: singleton + factory test =============================================\n";
+    {
+        Class_factory factory;
+
+        cout << (factory.register_class(CLSID_Class1, &create_singleton<Class, ICommon>) == false?"[main]: failed to register CLSID_Class1\n":"[main]: registered CLSID_Class1\n");
+
+        {
+            shared_ptr<IInterface> i1 = factory.create_object<IInterface>(CLSID_Class1);
+            cout << "[main]: i1 use_count: " << i1.use_count() << ", calling f1...\n";
+            i1->f1();
+            shared_ptr<IInterface> i2 = factory.create_object<IInterface>(CLSID_Class1);
+            cout << "[main]: i2 use_count: " << i2.use_count() << ", calling f1...\n";
+            i2->f1();
+        }
+    }
+
+    cout << "[main]: end\n";
     return 0;
 }
