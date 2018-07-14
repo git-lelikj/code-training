614656531812f331bf059200531f40d791376b4c
Alexei Jemkov <Alexei.Jemkov@advantechwireless.com>
2018-07-14 15:23:06 +0300

    Add active object

--- active_object_2.h
+++ active_object_2.h
@@ -19,30 +19,37 @@ public:
     void enqueue(T t)
     {
         lock_guard<mutex> lock(mutex_);
-        queue.push(move(t));
+        queue_.push(move(t));
+        not_empty_condition_.notify_one();
     }
 
     shared_ptr<T> wait_and_dequeue()
     {
         unique_lock<mutex> lock(mutex_);
-        not_empty_cond_.wait(lock, [this](){ return !queue_.empty() });
-        shared_ptr<T> res = make_shared<T>()
+        not_empty_condition_.wait(lock, [this](){ return !queue_.empty(); });
+        shared_ptr<T> res(make_shared<T>(move(queue_.front())));
+        queue_.pop();
+        return res;
     }
 
     void wait_and_dequeue(T& t)
     {
-
+        unique_lock<mutex> lock(mutex_);
+        not_empty_condition_.wait(lock, [this](){ return !queue_.empty(); });
+        t = move(queue_.front());
+        queue_.pop();
     }
 
-    bool empty()
+    bool empty() const
     {
-
+        lock_guard<mutex> lock(mutex_);
+        return queue_.empty();
     }
 
 private:
     queue<T> queue_;
     mutable mutex mutex_;
-    mutable condition_variable not_empty_cond_;
+    mutable condition_variable not_empty_condition_;
 };
 
 }
