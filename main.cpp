47e9c8293c47cb7917b3abbd73d20b325035942e
Alexei Jemkov <Alexei.Jemkov@advantechwireless.com>
2018-08-09 10:57:55 +0300

    Add new solutions

--- main.cpp
+++ main.cpp
@@ -2496,3 +2496,171 @@ vector<string> split_string(string input_string) {
     return splits;
 }
 #endif
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+//    Interview prep: Hash Tables: Ransom Note
+
+//    Sample Input 0
+//    6 4
+//    give me one grand today night
+//    give one grand today
+
+//    Sample Output 0
+//    Yes
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+#if 0
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the checkMagazine function below.
+void checkMagazine(vector<string> magazine, vector<string> note) {
+    unordered_multiset<string> magazine_hash;
+    for (const auto& s: magazine)
+        magazine_hash.insert(s);
+    for (const auto& s: note) {
+        auto it = magazine_hash.find(s);
+        if (it != magazine_hash.end())
+            magazine_hash.erase(it);
+        else {
+            cout << "No\n";
+            return;
+        }
+    }
+    cout << "Yes\n";
+    return;
+}
+
+int main()
+{
+    string mn_temp;
+    getline(cin, mn_temp);
+
+    vector<string> mn = split_string(mn_temp);
+
+    int m = stoi(mn[0]);
+
+    int n = stoi(mn[1]);
+
+    string magazine_temp_temp;
+    getline(cin, magazine_temp_temp);
+
+    vector<string> magazine_temp = split_string(magazine_temp_temp);
+
+    vector<string> magazine(m);
+
+    for (int i = 0; i < m; i++) {
+        string magazine_item = magazine_temp[i];
+
+        magazine[i] = magazine_item;
+    }
+
+    string note_temp_temp;
+    getline(cin, note_temp_temp);
+
+    vector<string> note_temp = split_string(note_temp_temp);
+
+    vector<string> note(n);
+
+    for (int i = 0; i < n; i++) {
+        string note_item = note_temp[i];
+
+        note[i] = note_item;
+    }
+
+    checkMagazine(magazine, note);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+#endif
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+//    Interview prep: Hash Tables: Two Strings
+
+//    Sample Input
+
+//    2
+//    hello
+//    world
+//    hi
+//    world
+//    Sample Output
+
+//    YES
+//    NO
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the twoStrings function below.
+string twoStrings(string s1, string s2) {
+    array<size_t, 255> letter_count = {};
+    for (auto c: s1)
+        ++letter_count[static_cast<size_t>(c)];
+    for (auto c: s2) {
+        if (letter_count[static_cast<size_t>(c)]) {
+            return "YES";
+        }
+    }
+    return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s1;
+        getline(cin, s1);
+
+        string s2;
+        getline(cin, s2);
+
+        string result = twoStrings(s1, s2);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
